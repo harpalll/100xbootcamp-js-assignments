ceb8292ec7094b45d0fb1af652a2a634d24dbca2
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2026-02-14 00:02:30 +0530

    feat: promises/easy/sumPromisses

--- Advanced-Javascript-Assignments/promises/easy/sumPromises.js
+++ Advanced-Javascript-Assignments/promises/easy/sumPromises.js
@@ -1,9 +1,14 @@
 // Problem Description – Sum of Two Promises
 
-// You are given two Promises that each resolve to numeric values. 
-// Your task is to return a new Promise that resolves to the sum of these two numbers. 
+// You are given two Promises that each resolve to numeric values.
+// Your task is to return a new Promise that resolves to the sum of these two numbers.
 // Both Promises should be executed in parallel using Promise.all to avoid unnecessary waiting.
-async function sumPromises(p1, p2) {}
+async function sumPromises(p1, p2) {
+  return new Promise(async (res, rej) => {
+    let [num1, num2] = await Promise.all([p1, p2]);
+
+    res(num1 + num2);
+  });
+}
 
 module.exports = sumPromises;
-
